0997f5f4b9a400c8868405219bb827481986b122
Kevin Timotius G <kevintg1212@gmail.com>
2025-09-10 08:32:49 +0700

    Resolve merge conflicts between local and origin/main 2

--- management/controller/conn_googleoauth.php
+++ management/controller/conn_googleoauth.php
@@ -4,7 +4,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $client = new Google_Client();
 $googleClientId = getenv('GOOGLE_CLIENT_ID');
 $googleClientSecret = getenv('GOOGLE_CLIENT_SECRET');
-$client->setRedirectUri('http://localhost:8080/mng_dik-main/management/callback.php');
+
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+$host     = $_SERVER['HTTP_HOST'];
+$basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+
+$redirectUri = $protocol . $host . $basePath . '/callback.php';
+
+$client->setRedirectUri($redirectUri);
+
 $client->addScope('email');
 $client->addScope('profile');
 
